eadcad70315e35279b859c3b00f2a241dca380b0
zhangbiao.168 <zhangbiao.168@bytedance.com>
2026-05-26 03:34:09 +0000

    Enable sequence parallelism (SP) for Qwen3.5 with FSDP2, covering the use_fused_kernels option as well as both Torch and Triton backends.

--- verl/models/transformers/monkey_patch.py
+++ verl/models/transformers/monkey_patch.py
@@ -490,11 +490,13 @@ def apply_monkey_patch(
             Qwen3_5ForConditionalGeneration,
             Qwen3_5Model,
             Qwen3_5VisionModel,
+            Qwen3_5TextModel
         )
         from transformers.models.qwen3_5_moe.modeling_qwen3_5_moe import (
             Qwen3_5MoeForConditionalGeneration,
             Qwen3_5MoeModel,
             Qwen3_5MoeVisionModel,
+            Qwen3_5MoeTextModel
         )
 
         from verl.models.transformers.qwen3_5 import (
@@ -512,6 +514,9 @@ def apply_monkey_patch(
         # Step 2: patch vision model to fix fsdp2 cpu_offload bug.
         Qwen3_5VisionModel.fast_pos_embed_interpolate = fast_pos_embed_interpolate
         Qwen3_5MoeVisionModel.fast_pos_embed_interpolate = fast_pos_embed_interpolate
+        if ulysses_sp_size > 1:
+            patch_vlm_for_ulysses_input_slicing(Qwen3_5TextModel)
+            patch_vlm_for_ulysses_input_slicing(Qwen3_5MoeTextModel)
 
     if use_remove_padding or ulysses_sp_size > 1:
         if hasattr(module, "_flash_attention_forward"):  # transformers <= 4.47.1 or legacy models
